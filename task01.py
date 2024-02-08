2de8028a15e4ef2c45c5cef05ae099ba1fb4d1ba
Anton Kobyshev <antonkobyshev@gmail.com>
2024-02-08 22:45:11 +0100

    Update task01.py

--- task01.py
+++ task01.py
@@ -37,6 +37,7 @@ class LinkedList:
 
     # Метод, який об'єднує два відсортовані списки в один
     def merge_sorted_lists(self, list2):
+
         merged_list = LinkedList()
         current1 = self.head
         current2 = list2.head
@@ -58,7 +59,7 @@ class LinkedList:
             current2 = current2.next
 
         return merged_list
-
+    
     # Метод, що відображає елементи списку
     def display(self):
         current_node = self.head
@@ -66,10 +67,56 @@ class LinkedList:
             print(current_node.data, end=" ")
             current_node = current_node.next
         print()
+        
+    # Основна функція
+    def merge_sort(self, head):
+        # Сортуємо список методом злиття
+        if head is None or head.next is None:
+            return head
 
-# Основна функція
+        middle = self.get_middle(head)
+        next_to_middle = middle.next
+        middle.next = None
+
+        left_half = self.merge_sort(head)
+        right_half = self.merge_sort(next_to_middle)
+
+        sorted_list = self.sorted_merge(left_half, right_half)
+        return sorted_list
+
+    def sorted_merge(self, left_half, right_half):
+        # Зливаємо два відсортованих підсписки
+        result = None
+
+        if left_half is None:
+            return right_half
+        if right_half is None:
+            return left_half
+
+        if left_half.data <= right_half.data:
+            result = left_half
+            result.next = self.sorted_merge(left_half.next, right_half)
+        else:
+            result = right_half
+            result.next = self.sorted_merge(left_half, right_half.next)
+
+        return result
+
+    def get_middle(self, head):
+        # Знаходимо середину списку
+        if head is None:
+            return head
+
+        slow_pointer = head
+        fast_pointer = head
+
+        while fast_pointer.next is not None and fast_pointer.next.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
+
 if __name__ == "__main__":
-    # Створення першого списку та вставлення елементів у відсортованому порядку
     linked_list = LinkedList()
     linked_list.sorted_insert(Node(7))
     linked_list.sorted_insert(Node(3))
@@ -79,12 +126,10 @@ if __name__ == "__main__":
     print("Original list:")
     linked_list.display()
 
-    # Реверсування першого списку
     linked_list.reverse()
     print("Reversed list:")
     linked_list.display()
 
-    # Створення другого списку та вставлення елементів у відсортованому порядку
     linked_list2 = LinkedList()
     linked_list2.sorted_insert(Node(4))
     linked_list2.sorted_insert(Node(8))
@@ -94,7 +139,6 @@ if __name__ == "__main__":
     print("Second list:")
     linked_list2.display()
 
-    # Об'єднання та відображення відсортованих списків
     merged_list = linked_list.merge_sorted_lists(linked_list2)
     print("Merged sorted lists:")
     merged_list.display()
